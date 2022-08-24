66567e04e844ab62750a223d684faefa5d95deb6
sukyungdev <sukyung.developer@gmail.com>
2022-08-25 02:13:29 +0900

    Feat : theme.html
    create theme
    theme.html
    style.css
    main.js

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ import './style.css';
 // TODO: Resolve User story #1 in Epic: CRUD
 // eslint-disable-next-line no-console
 console.log('e');
+
+// menu toggle
+const menuOnBtn = document.querySelector('.bar');
+const menuOffBtn = document.querySelector('.nav-off');
+const nav = document.querySelector('.nav-container');
+
+menuOnBtn.addEventListener('click', () => {
+	nav.classList.add('active');
+});
+menuOffBtn.addEventListener('click', () => {
+	nav.classList.remove('active');
+});
